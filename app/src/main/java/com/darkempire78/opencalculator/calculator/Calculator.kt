33943034f506b77b5ff2653bedda0240b13e2ca8
nezznee <javenno06@gmail.com>
2026-05-18 21:59:40 +0200

    Nevermind it can be NaN so !isFinite is needed..:
    1000.02! ->
    1. 1007.52.pow(1000.52) overflows to Infinity.
    2. exp(-1007.52) underflows to 0.0.
    3. Infinity * 0.0 results in NaN.

--- app/src/main/java/com/darkempire78/opencalculator/calculator/Calculator.kt
+++ app/src/main/java/com/darkempire78/opencalculator/calculator/Calculator.kt
@@ -76,7 +76,7 @@ class Calculator(
         val firstPart = sqrtTwoPi * t.pow(z + 0.5) * exp(-t)
         val result = firstPart * a
 
-        if (result.isInfinite()) {
+        if (!result.isFinite()) {
             is_infinity = true
             return BigDecimal.ZERO
         }
